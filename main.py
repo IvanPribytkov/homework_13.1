6e6366154a76e890aedef39e892c6981170efc50
IvanPribytkov <pribytkov.ivan@gmail.com>
2024-03-18 18:22:09 +0500

    Задание 2

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class Product:
+    total_products = 0  # Общее количество уникальных продуктов
+
     def __init__(self, name, description, price, quantity):
         """
         Инициализация объекта Product.
@@ -12,8 +14,12 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+        Product.total_products += 1  # Увеличиваем общее количество уникальных продуктов при создании нового продукта
 
 class Category:
+    total_categories = 0  # Общее количество категорий
+    total_unique_products = 0  # Общее количество уникальных продуктов (не учитывая количество в наличии)
+
     def __init__(self, name, description):
         """
         Инициализация объекта Category.
@@ -24,3 +30,14 @@ class Category:
         self.name = name
         self.description = description
         self.products = []  # Список товаров в данной категории
+        Category.total_categories += 1  # Увеличиваем общее количество категорий при создании новой категории
+
+    def add_product(self, product):
+        """
+        Добавление продукта в категорию.
+
+        :param product: Продукт для добавления.
+        """
+        self.products.append(product)
+        Category.total_unique_products += 1  # Увеличиваем общее количество уникальных продуктов при добавлении нового продукта в категорию
+
