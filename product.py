cdebfba5d26c708b3e6673c8f5cf93f71928bbc9
IvanPribytkov <pribytkov.ivan@gmail.com>
2024-03-21 18:42:42 +0500

    13.2 Режимы доступа. Задание 4

--- product.py
+++ product.py
@@ -10,18 +10,30 @@ class Product:
         """
         self.name = name
         self.description = description
-        self.price = price
+        self._price = price  # Приватный атрибут для цены
         self.quantity = quantity
 
-    @classmethod
-    def create(cls, name: str, description: str, price: float, quantity: int):
-        """
-        Создает и возвращает объект товара.
+    @property
+    def price(self):
+        """Геттер для цены."""
+        return self._price
 
-        :param name: Название товара.
-        :param description: Описание товара.
-        :param price: Цена товара.
-        :param quantity: Количество товара в наличии.
-        :return: Объект товара.
-        """
-        return cls(name, description, price, quantity)
+    @price.setter
+    def price(self, value):
+        """Сеттер для цены."""
+        if value <= 0:
+            print("Ошибка: Цена должна быть больше нуля.")
+        else:
+            self._price = value
+
+
+product = Product("Phone", "Smartphone", 500, 10)
+print(product.price)  # Выведет: 500
+
+# Установка новой корректной цены
+product.price = 600
+print(product.price)  # Выведет: 600
+
+# Попытка установить некорректную цену
+product.price = -100  # Выведет сообщение об ошибке
+print(product.price)  # Выведет: 600 (цена останется прежней)
