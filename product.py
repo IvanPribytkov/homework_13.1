160e54f3b4deef503936cb7ba4492a871e6053e3
IvanPribytkov <pribytkov.ivan@gmail.com>
2024-03-21 18:38:53 +0500

    13.2 Режимы доступа. Задание 3

--- product.py
+++ product.py
@@ -1,5 +1,4 @@
 class Product:
-
     def __init__(self, name: str, description: str, price: float, quantity: int):
         """
         Инициализация объекта Product.
@@ -13,3 +12,16 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def create(cls, name: str, description: str, price: float, quantity: int):
+        """
+        Создает и возвращает объект товара.
+
+        :param name: Название товара.
+        :param description: Описание товара.
+        :param price: Цена товара.
+        :param quantity: Количество товара в наличии.
+        :return: Объект товара.
+        """
+        return cls(name, description, price, quantity)
