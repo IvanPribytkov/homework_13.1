22d2faf93decacf633256fc538ef71d3ebc5b8f2
IvanPribytkov <pribytkov.ivan@gmail.com>
2024-03-21 17:09:46 +0500

    13.2 Режимы доступа. Задание 1

--- category.py
+++ category.py
@@ -4,17 +4,16 @@ class Category:
     total_categories = 0  # Общее количество категорий
     total_unique_products = 0  # Общее количество уникальных продуктов (не учитывая количество в наличии)
 
-    def __init__(self, name: str, description: str, products: list):
+    def __init__(self, name: str, description: str):
         """
         Инициализация объекта Category.
 
         :param name: Название категории.
         :param description: Описание категории.
-        :param products: Список товаров в данной категории.
         """
         self.name = name
         self.description = description
-        self.products = products
+        self._products = []  # Приватный атрибут - список товаров в данной категории
         # Увеличиваем общее количество категорий при создании новой категории
         Category.total_categories += 1
 
@@ -24,5 +23,5 @@ class Category:
 
         :param product: Продукт для добавления.
         """
-        self.products.append(product)
+        self._products.append(product)
         Category.total_unique_products += 1  # Увеличиваем общее количество уникальных продуктов при добавлении нового продукта в категорию
